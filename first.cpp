f19f06307d7931f02942d2e44bdb0c2cb3d1ef92
prasanthtalluri <62942404+prasanthtalluri@users.noreply.github.com>
2020-03-31 18:23:16 +0530

    initial stage

---
+++ first.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+int avt[10],but[10],ret[10],eT,i,s1;
+int r1=0,n1,t1,wait=0,turnaround=0;
+Sleep(2000);
+printf("loading..");Sleep(2000);
+printf("\nloading....");Sleep(2000);
+printf("\nloading......\n");Sleep(2000);
+printf("Your Software has been Successfully Loaded \n please tap ENTER button");
+getch();
+printf("\n<--------------------------Welcome To The Prasanth Scheduler Design Software------------------------>\n\n");
+Sleep(2000);
+printf("Please Read the following instructions carefully \n\n");Sleep(2000);
+printf("1.Do Not Enter FLOAT Values\n\n");
+Sleep(2000);
+printf("Enter number of Processes : ");
+scanf("%d",&n1);
+}
